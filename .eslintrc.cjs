05abcb4e1e0e5992e73d73cad8f6dbf6850dd14b
Léo Maradan <leo.maradan@bpa-solutions.net>
2022-02-02 09:40:51 +0100

    Update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,35 +1,42 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
   env: {
     browser: true,
     es2021: true,
   },
-  overrides: [
-    {
-      files: ["*.svelte"],
-      processor: "svelte3/svelte3",
-    },
-  ],
   parserOptions: {
     ecmaVersion: 2019,
     sourceType: "module",
-    tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json"],
-    extraFileExtensions: [".svelte"],
   },
-  plugins: ["svelte3", "@typescript-eslint"],
-  rules: {
-    "@typescript-eslint/no-unsafe-call": "off",
-    "@typescript-eslint/no-unsafe-assignment": "off",
-    "@typescript-eslint/no-unsafe-member-access": "off",
-  },
-  settings: {
-    "svelte3/typescript": true, // load TypeScript as peer dependency
-  },
-  extends: [
-    // then, enable whichever type-aware rules you want to use
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  overrides: [
+    {
+      files: ["*.ts, *.svelte"],
+      parser: "@typescript-eslint/parser",
+      overrides: [
+        {
+          files: ["*.svelte"],
+          processor: "svelte3/svelte3",
+        },
+      ],
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"],
+        extraFileExtensions: [".svelte"],
+      },
+      plugins: ["svelte3", "@typescript-eslint"],
+      rules: {
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+      },
+      settings: {
+        "svelte3/typescript": true, // load TypeScript as peer dependency
+      },
+      extends: [
+        // then, enable whichever type-aware rules you want to use
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+    },
   ],
 };
